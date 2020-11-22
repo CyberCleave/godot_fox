31b06bb2d1f766d7c3cdfba7cfd73ab34d5e7596
CyberCleave <cybercleave@gmail.com>
2020-11-22 18:32:38 +0300

    damage and back-off

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 
 signal power_updated(power)
+signal lives_updated(lives)
 
 const SPEED = 200.0
 const MAXSPEED = 500.0
@@ -10,20 +11,25 @@ const GRAVITY = 2000.0
 const JUMP_CUT_Y = 0.5
 const JUMP_CUT_X = 0.05
 const FLOOR = Vector2(0, -1)
-const FIREBALL = preload("res://Scenes/Items/Fireball.tscn")
+const FIREBALL = preload("res://Scenes/Misc/Fireball.tscn")
 const SHOT_COST = 20
 const DOUBLEJUMP_COST = 40
+const HIT_COST = 60
 
 var player_jump = false
 var can_jump = true
 var is_attacking = false
 var can_shoot = true
 var jump_pressed = false
+var being_hit = false
 var jump_count = 0
 
 var doublejump_limit = 2
+
+var max_lives = 99
 var max_power = 100
 onready var power = max_power setget _set_power
+onready var lives = 3 setget _set_lives
 
 var is_dead = false
 
@@ -31,6 +37,7 @@ var vel = Vector2()
 
 func _ready():
 	emit_signal("power_updated", power)
+	emit_signal("lives_updated", lives)
 
 func _physics_process(delta):
 	
@@ -43,119 +50,135 @@ func _physics_process(delta):
 			jump_count = 0
 			if jump_pressed:
 				vel.y = JUMPFORCE
-	
-		if Input.is_action_just_pressed("player_shoot") and !is_attacking and can_shoot:
-			if power >= SHOT_COST:
-				if is_on_floor():
-					if vel.x != 0:
-						$AnimatedSprite.play("run_attack")
-					else:
-						$AnimatedSprite.play("attack")
-				else:
-					$AnimatedSprite.play("air_attack")
-				is_attacking = true
-				can_shoot = false
-				shootTimer()
-				drain_power(SHOT_COST)
-				var fireball = FIREBALL.instance()
-				if sign($Position2D.position.x) == 1:
-					fireball.set_fireball_direction(1)
-				else:
-					fireball.set_fireball_direction(-1)
-				get_parent().add_child(fireball)
-				fireball.position = $Position2D.global_position
-				fireball.StartPosX = $Position2D.global_position.x
-			else:
-				print ("LOW POWER!")
-	
-		if Input.is_action_pressed("ui_left"):
-			$AnimatedSprite.flip_h = true
-			if sign($Position2D.position.x) == 1:
-				$Position2D.position.x *= -1
-			if vel.x >= 0:
-				vel.x = -SPEED;
-			if jump_count > 0:
-				vel.x = max(-MAXSPEED, vel.x)
-			if is_on_floor():
-				vel.x = max(-MAXSPEED, vel.x - SPEED * ACCELERATION)
-			if !is_attacking:
-				$AnimatedSprite.play("run")
-	
-		if Input.is_action_pressed("ui_right"):
-			$AnimatedSprite.flip_h = false
-			if sign($Position2D.position.x) == -1:
-				$Position2D.position.x *= -1
-			if vel.x <= 0:
-				vel.x = SPEED;
-			if jump_count > 0:
-				vel.x = min(MAXSPEED, vel.x)
-			if is_on_floor():
-				vel.x = min(MAXSPEED, vel.x + SPEED * ACCELERATION)
-			if !is_attacking:
-				$AnimatedSprite.play("run")
 				
-		elif (!Input.is_action_pressed("ui_left") and !Input.is_action_pressed("ui_right")) or (Input.is_action_pressed("ui_left") and Input.is_action_pressed("ui_right")):
-			if  is_on_floor():
-				if !is_attacking:
-					$AnimatedSprite.play("idle")
-				vel.x = 0
-			else:
-				vel.x = lerp (vel.x, 0, JUMP_CUT_X)
+		if (!being_hit):
+	
+			if Input.is_action_just_pressed("player_shoot") and !is_attacking and can_shoot:
+				if power >= SHOT_COST:
+					if is_on_floor():
+						if vel.x != 0:
+							$AnimatedSprite.play("run_attack")
+						else:
+							$AnimatedSprite.play("attack")
+					else:
+						$AnimatedSprite.play("air_attack")
+					is_attacking = true
+					can_shoot = false
+					shootTimer()
+					drain_power(SHOT_COST)
+					var fireball = FIREBALL.instance()
+					if sign($Position2D.position.x) == 1:
+						fireball.set_fireball_direction(1)
+					else:
+						fireball.set_fireball_direction(-1)
+					get_parent().get_parent().add_child(fireball)
+					fireball.position = $Position2D.global_position
+					fireball.StartPosX = $Position2D.global_position.x
+				else:
+					print ("LOW POWER!")
 		
-		if Input.is_action_just_pressed("player_jump"):
-			jump_pressed = true
-			jump_timer()
-			if can_jump:
-				jump_count += 1
-				vel.y = JUMPFORCE
-			
-		if !is_on_floor():
-			coyoteTime()
-			if vel.y < 0:
+			if Input.is_action_pressed("ui_left"):
+				$AnimatedSprite.flip_h = true
+				if sign($Position2D.position.x) == 1:
+					$Position2D.position.x *= -1
+				if vel.x >= 0:
+					vel.x = -SPEED;
+				if jump_count > 0:
+					vel.x = max(-MAXSPEED, vel.x)
+				if is_on_floor():
+					vel.x = max(-MAXSPEED, vel.x - SPEED * ACCELERATION)
 				if !is_attacking:
-					$AnimatedSprite.play("jump")
-					if Input.is_action_just_released("player_jump"):
-						vel.y = vel.y * JUMP_CUT_Y
-					if Input.is_action_just_pressed("player_jump"):
-						print("TOO EARLY!")
-			else:
+					$AnimatedSprite.play("run")
+		
+			if Input.is_action_pressed("ui_right"):
+				$AnimatedSprite.flip_h = false
+				if sign($Position2D.position.x) == -1:
+					$Position2D.position.x *= -1
+				if vel.x <= 0:
+					vel.x = SPEED;
+				if jump_count > 0:
+					vel.x = min(MAXSPEED, vel.x)
+				if is_on_floor():
+					vel.x = min(MAXSPEED, vel.x + SPEED * ACCELERATION)
 				if !is_attacking:
-					$AnimatedSprite.play("fall")
-					if Input.is_action_just_pressed("player_jump"):
-							if jump_count > 0 && jump_count < doublejump_limit:
-								if vel.y < -JUMPFORCE / 2 - GRAVITY * delta:
-									if power >= DOUBLEJUMP_COST:
-										vel.y = JUMPFORCE
-										jump_count += 1
-										drain_power(DOUBLEJUMP_COST)
-										print("DOUBLE JUMP!")
-									else:
-										print("LOW POWER!")
-								else:
-									print("TOO LATE!")
-							else:
-								print("FALLING FROM AN EDGE OR DOUBLE JUMP LIMIT EXCEEDED!")
-
-			if is_on_ceiling():
-				vel.y = 0
-				jump_pressed = false
+					$AnimatedSprite.play("run")
 					
-		if get_slide_count() > 0:
-			for i in range(get_slide_count()):
-				if "Enemy" in get_slide_collision(i).collider.name:
-					get_slide_collision(i).collider.attack()
-					dead()
-				if "Spring_Shroom" in get_slide_collision(i).collider.name:
-					get_slide_collision(i).collider.spring()
-					spring()
+			elif (!Input.is_action_pressed("ui_left") and !Input.is_action_pressed("ui_right")) or (Input.is_action_pressed("ui_left") and Input.is_action_pressed("ui_right")):
+				if  is_on_floor():
+					if !is_attacking:
+						$AnimatedSprite.play("idle")
+					vel.x = 0
+				else:
+					vel.x = lerp (vel.x, 0, JUMP_CUT_X)
+			
+			if Input.is_action_just_pressed("player_jump"):
+				jump_pressed = true
+				jump_timer()
+				if can_jump:
+					jump_count += 1
+					vel.y = JUMPFORCE
+				
+			if !is_on_floor():
+				coyoteTime()
+				if vel.y < 0:
+					if !is_attacking:
+						$AnimatedSprite.play("jump")
+						if Input.is_action_just_released("player_jump"):
+							vel.y = vel.y * JUMP_CUT_Y
+						if Input.is_action_just_pressed("player_jump"):
+							print("TOO EARLY!")
+				else:
+					if !is_attacking:
+						$AnimatedSprite.play("fall")
+						if Input.is_action_just_pressed("player_jump"):
+								if jump_count > 0 && jump_count < doublejump_limit:
+									if vel.y < -JUMPFORCE / 2 - GRAVITY * delta:
+										if power >= DOUBLEJUMP_COST:
+											vel.y = JUMPFORCE
+											jump_count += 1
+											drain_power(DOUBLEJUMP_COST)
+											print("DOUBLE JUMP!")
+										else:
+											print("LOW POWER!")
+									else:
+										print("TOO LATE!")
+								else:
+									print("FALLING FROM AN EDGE OR DOUBLE JUMP LIMIT EXCEEDED!")
+	
+				if is_on_ceiling():
+					vel.y = 0
+					jump_pressed = false
+						
+			if get_slide_count() > 0:
+				for i in range(get_slide_count()):
+					if "Enemy" in get_slide_collision(i).collider.name:
+						get_slide_collision(i).collider.attack()
+						if (!being_hit):
+							hit()
+					if "Spring_Shroom" in get_slide_collision(i).collider.name:
+						get_slide_collision(i).collider.spring()
+						spring()
 	
 	
 	vel.y += GRAVITY * delta
 		
 	vel = move_and_slide(vel, FLOOR)
-		
+	
 func spring():
 	vel.y += JUMPFORCE
+	
+func hit():
+	print("BEING HIT!")
+	if power >= HIT_COST:
+		being_hit = true
+		if vel.x >= 0:
+			vel.x = -SPEED
+			vel.y = JUMPFORCE / 2
+		drain_power(HIT_COST)
+		yield(get_tree().create_timer(.3), "timeout")
+		being_hit = false
+	else:
+		dead()
 		
 func dead():
 	set_collision_mask(8)
@@ -183,15 +206,34 @@ func jump_timer():
 	jump_pressed = false
 
 func _on_Timer_timeout():
-	var error_code = get_tree().change_scene("res://Scenes/Menus/TitleScreen.tscn")
-	if error_code != 0:
-		print("ERROR: ", error_code)
+	loss_lives(1)
+	if lives > 0:
+		respawn()
+	else:
+		var error_code = get_tree().change_scene("res://Scenes/Menus/TitleScreen.tscn")
+		if error_code != 0:
+			print("ERROR: ", error_code)
 		
 func drain_power(amount):
 	_set_power(power - amount)
+	
+func loss_lives(amount):
+	_set_lives(lives - amount)
 		
 func _set_power(value):
 	var prev_power = power
 	power = clamp(value, 0, max_power)
 	if power != prev_power:
 		emit_signal("power_updated", power)
+		
+func _set_lives(value):
+	var prev_lives = lives
+	lives = clamp(value, 0, max_lives)
+	if lives != prev_lives:
+		emit_signal("lives_updated", lives)
+		
+func respawn():
+	is_dead = false
+	set_collision_mask(15)
+	set_collision_layer(1)
+	global_position = get_parent().global_position
